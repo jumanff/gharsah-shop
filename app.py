bb35127a0e69ccc15d2ec1a8894c503aa0f15424
jumanff <joman_f@hotmail.com>
2026-06-13 18:09:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
 stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER', 'sandbox.smtp.mailtrap.io')
+app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 2525))
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')  
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')        
-app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = 'support@gharsah.shop'
 
 mail = Mail(app)
 s = URLSafeTimedSerializer(app.secret_key)
